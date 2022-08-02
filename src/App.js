8ed9440cb2688747414e75796bc9912a7aed6975
sagegod <cdeng1999@gmail.com>
2022-08-01 23:56:44 -0700

    added some pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import BucketList from './components/BucketList';
+
+
+function App() {
+    return (
+      <div className="bucket-app">
+        <p>hello</p>
+      </div>
+    );
+  }
+  
+  export default App;
