40dcd4c50c311b6357c0b511177a66394a7c7e59
sagegod <cdeng1999@gmail.com>
2022-08-02 00:08:27 -0700

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  ApolloProvider } from '@apollo/client';
+
 import './App.css';
-// import BucketList from './components/BucketList';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Aboutme from './pages/Aboutme';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 
 function App() {
-    return (
-      <div className="bucket-app">
-        <p>hello</p>
-      </div>
+  return (
+    <ApolloProvider client={client}>
+    <Router>
+    <Header />   
+    <div className="flex-column justify-center align-center min-100-vh bg-primary">
+    <Routes>
+            <Route 
+              path="/" 
+              element={<Aboutme />}
+            />
+            <Route 
+              path="/Portfolio" 
+              element={<Portfolio />}
+            />
+            <Route 
+              path="/Contact" 
+              element={<Contact />}
+            />
+            <Route 
+              path="/Resume"
+              element={<Resume />}
+            />
+          </Routes>
+    </div>
+    </Router>
+    <Footer />
+    </ApolloProvider>
     );
   }
   
